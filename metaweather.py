4dc466b44123a8a00a4ebc82bb22af74d8b87f9b
Qiyun Dai <pianoquin@gmail.com>
2020-04-09 13:34:39 -0500

    Deleted print(ID) line

--- metaweather.py
+++ metaweather.py
@@ -3,12 +3,18 @@ import requests
 def select(result):
     for i in range(len(result)):
         r_title = result[i]['title']
-        is_it = input(f'did you mean {r_title}?')
+        temp_id = result[i]['woeid']
+        assume = requests.get(f'https://www.metaweather.com/api/location/{temp_id}')
+        state = assume.json()['parent']['title']
+        is_it = input(f'did you mean {r_title} in {state}?')
         if 'y' in is_it:
             return result[i]['woeid']
+
 def confirmed_location(result2):
     cons_dict = result2.json()
-    return cons_dict['title']
+    city = cons_dict['title']
+    state = cons_dict['parent']['title']
+    return f'{city} in {state}'
 
 def find_temp(result2):
     cons_dict = result2.json()
@@ -24,12 +30,11 @@ loca = {'query': location}
 r1 = requests.get("https://www.metaweather.com/api/location/search/", params=loca)
 
 r_list = r1.json()
-print(r_list)
 
-# ID = select(r_list)
+ID = select(r_list)
+print(ID)
+r2 = requests.get(f'https://www.metaweather.com/api/location/{ID}')
 
-# r2 = requests.get(f'https://www.metaweather.com/api/location/{ID}')
+current_temp = find_temp(r2)
 
-# current_temp = find_temp(r2)
-
-# print(f'Found it for you, my dear. The current temperature at {confirmed_location(r2)} is C°{current_temp}.')
+print(f'Found it for you, my dear. The current temperature at {confirmed_location(r2)} is C°{current_temp}.')
